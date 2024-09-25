ed40b98480ff61511449dd0565e27674d3bcb7cd
ApinisMikelis <mikelis@tests-MacBook-Pro.local>
2024-09-25 13:50:49 +0300

    Revert "Replaced footer contacts with link to dealer page"
    This reverts commit 5c6fea906d95cbf8a34bdb36a83b44374b14c0df.

--- footer.php
+++ footer.php
@@ -12,9 +12,13 @@
 </div>
 <footer id="colophon" role="contentinfo">
     <div class="footer-contacts">
-        <a href="https://trenazieri.lv">
-            Contact dealer
-        </a>
+        <ul>
+            <li class="address">Biekensalas 6, Riga, Latvia</li>
+            <li class="phone">+371 67845794, +371 26625388</li>
+            <li class="mail"><a href="mailto:info@sportsystems.lv">info@sportsystems.lv</a></li>
+            <li class="facebook"><a target="_blank" href="https://www.facebook.com/SportsystemsLtd">SportsystemsLtd</a></li>
+            <li class="skype"><a href="skype:sport.systems?call">sport.systems (click to call us)</a></li>
+        </ul>
     </div>
     <div class="footer-menus">
         <div class="general-map">
