bc14d8f5d5fa21c56f4cb31b9d40580ed01a3c86
ApinisMikelis <m.apinis92@gmail.com>
2024-09-25 13:53:26 +0300

    Replaced footer contacts with link to dealer page

--- footer.php
+++ footer.php
@@ -12,13 +12,9 @@
 </div>
 <footer id="colophon" role="contentinfo">
     <div class="footer-contacts">
-        <ul>
-            <li class="address">Biekensalas 6, Riga, Latvia</li>
-            <li class="phone">+371 67845794, +371 26625388</li>
-            <li class="mail"><a href="mailto:info@sportsystems.lv">info@sportsystems.lv</a></li>
-            <li class="facebook"><a target="_blank" href="https://www.facebook.com/SportsystemsLtd">SportsystemsLtd</a></li>
-            <li class="skype"><a href="skype:sport.systems?call">sport.systems (click to call us)</a></li>
-        </ul>
+        <a href="https://trenazieri.lv">
+            Contact dealer
+        </a>
     </div>
     <div class="footer-menus">
         <div class="general-map">
